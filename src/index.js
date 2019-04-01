28d123114279a0e6bfe546bda2196e0cdacaaad0
Cristian Atehortua <cristian25a32@gmail.com>
2019-04-01 14:25:00 -0500

    Added sass and reactstrap (bootstrap)

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.scss';
 
 ReactDOM.render(React.createElement(App), document.getElementById('root'));
 
