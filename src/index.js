824957d390127ad376037d8b0a9499d0f2ac415f
Cristian Atehortua <cristian25a32@gmail.com>
2019-04-01 14:27:58 -0500

    Added Router as parent component of App

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 /* eslint-env browser */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -8,7 +9,11 @@ import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.scss';
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(
+  React.createElement(
+    Router, null, React.createElement(App),
+  ), document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
